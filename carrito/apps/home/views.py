59bd8633358e239a4b670018e986995bdd306064
Unknown <jdibicho@gmail.com>
2018-11-21 15:49:18 -0600

    lll

---
+++ carrito/apps/home/views.py
@@ -0,0 +1,111 @@
+#importamos la loibrerias de aceso de django
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+#impostamos la libreria models y traemos la clase productos para crear nuestra vista dinamica desde la db
+from carrito.apps.ventas.models import producto
+#importar el formulario creado
+from carrito.apps.home.forms import ContactForm, LoginForm, RegisterForm
+#libreria para enviar correo con estilo html
+from django.core.mail import EmailMultiAlternatives
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+from django.http import HttpResponseRedirect
+#libreria de paginacion django
+from django.core.paginator import Paginator,EmptyPage,InvalidPage
+
+#creamos nuestra vistas
+def index_view(request):
+	return render_to_response('home/index.html',context_instance=RequestContext(request))
+
+def about_view(request):
+	mensaje = 'Mensaje enviado desde mi vista'
+	ctx = {'msg':mensaje}
+	return render_to_response('home/about.html',ctx,context_instance=RequestContext(request))
+
+def productos_view(request,pagina):
+	#select * from ventas_productos where status = True ORDER BY id DESC
+	lista_prod = producto.objects.filter(status=True).order_by('-id')
+	paginator = Paginator(lista_prod,10)
+	try:
+		page = int(pagina)
+	except:
+		page = 1
+	try:
+		productos = paginator.page(page)
+	except (EmptyPage,InvalidPage):
+		productos = paginator.page(paginator.num_pages)
+	ctx = {'productos':productos}
+	return render_to_response('home/productos.html',ctx,context_instance=RequestContext(request))
+
+def singleProduct_view(request,id_prod):
+	prod = producto.objects.get(id=id_prod)
+	cats = prod.categorias.all() # Obteniendo las categorias del producto encontrado
+	ctx = {'producto':prod,'categorias':cats}
+	return render_to_response('home/singleProducto.html',ctx,context_instance=RequestContext(request))
+
+def contacto_view(request):
+	info_enviado = False
+	email  = ""
+	titulo = ""
+	texto  = ""
+	if request.method == "POST":
+		formulario = ContactForm(request.POST)
+		if formulario.is_valid():
+			info_enviado = True
+			email = formulario.cleaned_data['Email']
+			titulo = formulario.cleaned_data['Titulo']
+			texto = formulario.cleaned_data['Texto']
+
+			#configuracion de enviar correo
+			to_admin = 'tucorrreo@gmail.com'
+			html_content = "Imformacion recibida de %s<br><br><br>***Mensaje***<br><br>%s"%(email,texto)
+			msg = EmailMultiAlternatives('Correo de contacto', html_content,'from@server.com',[to_admin])
+			msg.attach_alternative(html_content,'text/html') #define el content html
+			msg.send() #enviar
+
+	else:
+		formulario = ContactForm()
+	ctx = {'form':formulario, 'email':email,'titulo':titulo, 'texto':texto, 'info_enviado':info_enviado}
+	return render_to_response('home/contacto.html',ctx,context_instance=RequestContext(request))
+
+def login_view(request):
+	mensaje = ""
+	if request.user.is_authenticated():
+		     return HttpResponseRedirect('/')
+	else:
+     	     if request.method == "POST":
+     	     	form = LoginForm(request.POST)
+     	     	if form.is_valid():
+     	     		username = form.cleaned_data['username']
+     	     		password = form.cleaned_data['password']
+     	     		usuario = authenticate(username=username,password=password)
+     	     		if usuario is not None and usuario.is_active:
+     	     			login(request,usuario)
+     	     			return HttpResponseRedirect('/')
+     	     		else:
+     	     			mensaje = "Usuario y/o password icorrecto :("
+             form = LoginForm()
+             ctx = {'form':form, 'mensaje':mensaje}
+             return render_to_response('home/login.html', ctx, context_instance=RequestContext(request))
+
+def logout_view(request):
+	logout(request)
+	return HttpResponseRedirect('/')
+
+def register_view(request):
+	form = RegisterForm()
+	if request.method == "POST":
+		form = RegisterForm(request.POST)
+		if form.is_valid():
+			usuario = form.cleaned_data['username']
+			email = form.cleaned_data['email']
+			password_one = form.cleaned_data['password_one']
+			password_two = form.cleaned_data['password_two']
+			u = User.objects.create_user(username=usuario,email=email,password=password_one)
+			u.save()
+			return render_to_response('home/register_ok.html',context_instance=RequestContext(request))
+      	else:
+      		ctx = {'form':form}
+      		return render_to_response('home/register.html',ctx,context_instance=RequestContext(request))
+	ctx = {'form':form}
+	return render_to_response('home/register.html',ctx,context_instance=RequestContext(request))
---
+++ carrito/apps/ventas/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class cliente(models.Model):
+	       nombre         = models.CharField(max_length=50)
+	       apellidos      = models.CharField(max_length=50)
+	       status         = models.BooleanField(default=True)
+
+	       def __unicode__(self):
+	       	      nombreCompleto = "%s %s"%(self.nombre,self.apellidos)
+	       	      return nombreCompleto
+	       	      #retorna la concatenacion del nombre y el apellido para mostrar mas la descripcion del cliente en el panel
+
+class categoriaProducto(models.Model):
+	nombre = models.CharField(max_length=50)
+	descripcion = models.TextField(max_length=400)
+
+	def __unicode__(self):
+		return self.nombre
+
+class producto(models.Model):
+	def url(self,filename):
+		ruta = "MultimediaData/producto/%s/%s"%(self.nombre,str(filename))
+		return ruta
+
+	nombre      = models.CharField(max_length=100)
+	descripcion = models.TextField(max_length=300)
+	status      = models.BooleanField(default=True)
+	imagen      = models.ImageField(upload_to=url,null=True,blank=True)
+	precio      = models.DecimalField(max_digits=6,decimal_places=2)
+	stock       = models.IntegerField()
+	categorias  = models.ManyToManyField(categoriaProducto,null=True,blank=True)
+
+	def __unicode__(self):
+		return self.nombre
+    	#retornar nombre del producto para presentar una descripcion en el panel
